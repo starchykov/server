c37202d56ecd8b779acc13c0a267f5e9a6b1b2e5
Ivan <zeesnippi@gmail.com>
2021-01-10 19:36:32 +0200

    server

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const http = require('http');
-const app = require('./app');
-const port = 6001;
-
-const server = http.createServer(app);
-
-server.listen(port,() => console.log(`Server is working on port ${port}`));
-
+// const http = require('http');
 // const app = require('./app');
-// const port = process.env.PORT || 3006
+// const port = 6001;
 //
-// //server listening
-// app.listen(port, () => console.log(`Server is working on port ${port}`)
-// );
+// const server = http.createServer(app);
+//
+// server.listen(port,() => console.log(`Server is working on port ${port}`));
+
+const app = require('./app');
+const port = process.env.PORT || 6001
+
+//server listening
+app.listen(port, () => console.log(`Server is working on port ${port}`)
+);
