43c70644008885985948be5248adc56510882c4c
Ivan <zeesnippi@gmail.com>
2021-01-15 22:18:04 +0200

    server

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
+var path = require('path')
+var serveStatic = require('serve-static')
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
 
 const app = express();
+app.use(serveStatic(path.join(__dirname, 'dist')))
 
 app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
 app.use(morgan('dev'));
@@ -15,9 +18,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use('/uploads', express.static('uploads'));
 
-app.use("/posts", postsRoute);
 app.use("/user", userRoute);
-app.use("/comments", commentsRoute);
-app.use("/images", imageRoute);
 
 module.exports = app;
