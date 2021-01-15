2b2d7d097df4ccca9055a71896e7bf127d5b1d8d
Ivan <zeesnippi@gmail.com>
2021-01-15 22:50:31 +0200

    server

--- app.js
+++ app.js
@@ -8,13 +8,18 @@ const app = express();
 app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
 app.use(morgan('dev'));
 
-
+const postsRoute = require('./routes/posts');
 const userRoute = require('./routes/user');
+const commentsRoute = require('./routes/comments');
+const imageRoute = require('./routes/images');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use('/uploads', express.static('uploads'));
 
+app.use("/posts", postsRoute);
 app.use("/user", userRoute);
+app.use("/comments", commentsRoute);
+app.use("/images", imageRoute);
 
 module.exports = app;
