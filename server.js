7beb67f88d479f26f654e6f8037d36eecd64a0ff
Ivan <zeesnippi@gmail.com>
2021-01-15 19:26:38 +0200

    server

--- server.js
+++ server.js
@@ -7,7 +7,7 @@
 // server.listen(port,() => console.log(`Server is working on port ${port}`));
 
 const app = require('./app');
-const port = process.env.PORT || 6001
+const port = process.env.PORT || 8000
 
 //server listening
 app.listen(port, () => console.log(`Server is working on port ${port}`)
