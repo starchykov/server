889f282a1f44063d8ec852c99ecf3f59e1dd9ac8
Ivan <zeesnippi@gmail.com>
2021-01-17 04:43:22 +0200

    Router

--- app.js
+++ app.js
@@ -24,12 +24,13 @@ app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
-app.use('/', (req, res) => { res.sendFile(path.join(__dirname, 'build/index.html'))});
+
 
 app.use('/uploads', express.static('uploads'));
 app.use("/posts", postsRoute);
 app.use("/user", userRoute);
 app.use("/comments", commentsRoute);
 app.use("/images", imageRoute);
+app.use('/', (req, res) => { res.sendFile(path.join(__dirname, 'build/index.html'))});
 
 module.exports = app;
